8273b0087455eeaee5d4c94ad1b273fa9bd80523
Rajesh P <rajesh.pethe@gmail.com>
2019-11-11 19:09:07 +0530

    Force add primary key for SQL Alchemy to work.

--- application/models.py
+++ application/models.py
@@ -1,14 +1,14 @@
 from . import db
 
 
-class Gene_Autocomplete(db.Model):
+class Genes(db.Model):
     
     __tablename__ = 'gene_autocomplete'
     __table_args__ = {'extend_existing': True}
     
     species = db.Column(db.String(255))
     display_label = db.Column(db.String(128))
-    stable_id = db.Column(db.String(128))
+    stable_id = db.Column(db.String(128), primary_key=True)
     location = db.Column(db.String(60))
 
     def __repr__(self):
